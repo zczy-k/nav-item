ca75ec30f9803d7a1760f0c29f610bb99a917902
zczy-k <z320952666@gmail.com>
2025-11-12 12:14:39 +0800

    refactor: 添加 .dockerignore 并清理未使用的依赖

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
 const db = require('./db');
 const menuRoutes = require('./routes/menu');
 const cardRoutes = require('./routes/card');
